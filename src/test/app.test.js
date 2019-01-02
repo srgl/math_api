c0ee01328f40bf8b6fb8f92cb7f35acde951ee9c
Sergei L <srglbnv@gmail.com>
2019-01-03 01:22:57 +0400

    Add tests for api

---
+++ src/test/app.test.js
@@ -0,0 +1,67 @@
+// @flow
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import Ajv from 'ajv';
+import app from '../app';
+
+chai.use(chaiHttp);
+const ajv = new Ajv();
+const errorResponse = {
+  type: 'object',
+  required: ['error'],
+  properties: {
+    error: { type: 'string' },
+  },
+};
+const divResponse = {
+  type: 'object',
+  required: ['result'],
+  properties: {
+    result: { type: 'number' },
+  },
+};
+const sqrtResponse = {
+  type: 'object',
+  required: ['result'],
+  properties: {
+    result: { type: 'array', items: { type: 'number' } },
+  },
+};
+
+describe('Math API', () => {
+  describe('/div', () => {
+    it('return correct division result', async () => {
+      // $FlowFixMe
+      const res = await chai.request(app).get('/div?a=-1&b=2');
+      expect(res.status).to.eq(200);
+      expect(ajv.validate(divResponse, res.body)).to.eq(true);
+      expect(res.body.result).to.eq(-0.5);
+    });
+    it('do not accept zero as divisor', async () => {
+      // $FlowFixMe
+      const res = await chai.request(app).get('/div?a=1&b=0');
+      expect(res.status).to.eq(400);
+      expect(ajv.validate(errorResponse, res.body)).to.eq(true);
+      expect(res.body.error).to.eq('validation_error');
+    });
+  });
+
+  describe('/sqrt', () => {
+    it('return correct sqrt result', async () => {
+      // $FlowFixMe
+      const res = await chai.request(app).post('/sqrt')
+        .send({ data: [1, 4, 9] });
+      expect(res.status).to.eq(200);
+      expect(ajv.validate(sqrtResponse, res.body)).to.eq(true);
+      expect(res.body.result).to.deep.equal([1, 2, 3]);
+    });
+    it('do not accept negative numbers', async () => {
+      // $FlowFixMe
+      const res = await chai.request(app).post('/sqrt')
+        .send({ data: [1, -4, 9] });
+      expect(res.status).to.eq(400);
+      expect(ajv.validate(errorResponse, res.body)).to.eq(true);
+      expect(res.body.error).to.eq('validation_error');
+    });
+  });
+});
